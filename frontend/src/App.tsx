ad83d1f6992383e7131d58b544fe87ea633c201c
Abd2301 <149315902+Abd2301@users.noreply.github.com>
2025-06-30 14:35:47 +0530

    removed unsued imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { CssBaseline, Container, Alert, Box, Paper } from '@mui/material';
+import { CssBaseline, Container, Box, Paper } from '@mui/material';
 import TaskList from './components/TaskList';
 
 // Create a client
